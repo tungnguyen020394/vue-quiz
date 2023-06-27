21bbb859e1c51b2967399446372b50367f196025
Nguyen Tung Nguyen <NguyenNT@vietis.com.vn>
2023-06-27 10:54:41 +0700

    update logic code

--- src/components/Questions.vue
+++ src/components/Questions.vue
@@ -1,13 +1,14 @@
 <template>
     <div class="questions-ctr">
         <div class="progress">
-            <div class="bar"></div>
-            <div class="status">1 out of 3 questions answered</div>
+            <div class="bar" :style="{ width: `${(questionsAnswered / questions.length) * 100}%` }"></div>
+            <div class="status">{{ questionsAnswered }} out of {{ questions.length }} questions answered</div>
         </div>
-        <div class="single-question" v-for="question in questions" :key="question">
+        <div class="single-question" v-for="(question, qi) in questions" :key="question" v-show="questionsAnswered == qi">
             <div class="question">{{ question.q }}</div>
             <div class="answers">
-                <div class="answer" v-for="answer in question.answers" :key="answer">{{ answer.text }}</div>
+                <div class="answer" v-for="answer in question.answers" :key="answer"
+                    @click.prevent="selectAnswer(answer.is_correct)">{{ answer.text }}</div>
             </div>
         </div>
     </div>
@@ -16,7 +17,13 @@
 <script>
 export default {
     name: 'Question',
-    props: ['questions']
+    props: ['questions', 'questionsAnswered'],
+    emits: ['question-answered'],
+    methods: {
+        selectAnswer(is_correct) {
+            this.$emit('question-answered', is_correct)
+        }
+    }
 }
 </script>
 
